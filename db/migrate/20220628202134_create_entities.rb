23350ef0b4a25fa8263be62818b763b097730f73
Estebanmual <esteban.munozal@gmail.com>
2022-06-28 22:21:30 -0500

    Add references between tables

--- db/migrate/20220628202134_create_entities.rb
+++ db/migrate/20220628202134_create_entities.rb
@@ -1,7 +1,8 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-
+      t.string :name, null: false
+      t.money :amount, default: 0, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_202134) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_030118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon", default: "https://source.unsplash.com/random/?city,night"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "entities", force: :cascade do |t|
+    t.string "name", null: false
+    t.money "amount", scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_entities_on_category_id"
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -36,4 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_202134) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "entities", "categories"
+  add_foreign_key "entities", "users"
 end
