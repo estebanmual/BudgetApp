2a2eb7cfbc6c595587406d41499434766ff36003
Estebanmual <esteban.munozal@gmail.com>
2022-06-29 17:25:15 -0500

    Added and refactor actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,16 @@
 class CategoriesController < ApplicationController
+  before_action :redirect
+  skip_before_action :redirect, only: [:splash]
   before_action :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.includes(:entities).all
   end
 
   # GET /categories/1 or /categories/1.json
   def show
+    @entities = @category.entities.order(created_at: :desc)
   end
 
   # GET /categories/new
@@ -19,18 +22,13 @@ class CategoriesController < ApplicationController
   def edit
   end
 
-  # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to categories_path, notice: "Category was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -57,7 +55,16 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def splash
+    redirect_to categories_path if user_signed_in?
+  end
+
   private
+
+    def redirect
+      redirect_to root_path unless user_signed_in?
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
@@ -65,6 +72,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.fetch(:category, {})
+      params.require(:category).permit(:name, :icon)
     end
 end
