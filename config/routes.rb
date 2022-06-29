07d88e58afe7a7e69e420a68ad835887f6475157
Estebanmual <esteban.munozal@gmail.com>
2022-06-28 21:19:36 -0500

    Added controllers, views, routes and models for categories and entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'categories#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories do
+    resources :entities  
+  end
+  devise_for :users
 end
