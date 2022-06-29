eda02bda6e9e8f6cb443d86aefe106bda6862b45
Estebanmual <esteban.munozal@gmail.com>
2022-06-29 17:40:42 -0500

    Change root file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'categories#index'
-
-  resources :categories do
-    resources :entities  
-  end
+  root 'categories#splash'
   devise_for :users
+  resources :categories do
+    resources :entities
+  end
 end
