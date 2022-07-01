52e5f55bd24fd14fbb3209fcf12537eed2c9f31a
Estebanmual <esteban.munozal@gmail.com>
2022-07-01 11:04:11 -0500

    Add new categories view test

--- spec/views/categories/new.html.erb_spec.rb
+++ spec/views/categories/new.html.erb_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'categories/new', type: :view do
+RSpec.describe 'categories/new', type: :feature do
   before(:each) do
-    assign(:category, Category.new)
+    visit new_user_session_path
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    click_button 'NEW CATEGORY'
   end
 
   it 'renders new category form' do
-    render
+    expect(page).to have_selector('form.newCategory__form')
+  end
 
-    assert_select 'form[action=?][method=?]', categories_path, 'post' do
-    end
+  it 'renders the navbar' do
+    expect(page).to have_selector('nav.navbar')
+  end
+
+  it 'take me back to categories index' do
+    click_on '<'
+    expect(current_path).to eq('/categories')
   end
 end
