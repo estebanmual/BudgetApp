f2b18ba5f2490d4c71d29e491f4096cb9e421e05
Estebanmual <esteban.munozal@gmail.com>
2022-07-01 09:11:50 -0500

    Add entity model tests

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Entity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid' do
+    entity = Entity.new(name: "Test", amount: 100, user_id: 1, category_id: 1)
+    expect(entity).to be_valid
+  end
+
+  it 'validates presence of name' do
+    entity = Entity.new(amount: 100, user_id: 1, category_id: 1)
+    expect(entity).to_not be_valid
+  end
+
+  it 'validates presence of amount' do
+    entity = Entity.new(name: "Test", user_id: 1, category_id: 1)
+    expect(entity).to be_valid
+  end
 end
