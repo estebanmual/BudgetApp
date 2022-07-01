3e2970ca2771a209dcc5930bcc256d188acb0533
Estebanmual <esteban.munozal@gmail.com>
2022-07-01 10:56:18 -0500

    Add categories' index view test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-user1 = User.create(id: 1, name: "John Doe", email: "john@example.com", password: "password")
+user1 = User.create(name: "John Doe", email: "john@example.com", password: "password")
 
-category1 = Category.create(id: 1, name: "Food", icon: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80", user_id: 1)
-category2 = Category.create(id: 2, name: "Transport", icon: "https://images.unsplash.com/photo-1613638377394-281765460baa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80", user_id: 1)
-category3 = Category.create(id: 3, name: "Entertainment", icon: "https://images.unsplash.com/photo-1586899028174-e7098604235b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80", user_id: 1)
+category1 = Category.create(name: "Food", icon: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80", user_id: 1)
+category2 = Category.create(name: "Transport", icon: "https://images.unsplash.com/photo-1613638377394-281765460baa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80", user_id: 1)
+category3 = Category.create(name: "Entertainment", icon: "https://images.unsplash.com/photo-1586899028174-e7098604235b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80", user_id: 1)
 
-entity1 = Entity.create(id: 1, name: "Mcdonalds", amount: 6, user_id: 1, category_id: 1)
-entity2 = Entity.create(id: 2, name: "Uber", amount: 13, user_id: 1, category_id: 2)
-entity3 = Entity.create(id: 3, name: "Didi", amount: 12, user_id: 1, category_id: 2)
-entity4 = Entity.create(id: 4, name: "Netflix", amount: 10, user_id: 1, category_id: 3)
-entity5 = Entity.create(id: 5, name: "Spotify", amount: 10, user_id: 1, category_id: 3)
-entity6 = Entity.create(id: 6, name: "Starbucks", amount: 5, user_id: 1, category_id: 1)
+entity1 = Entity.create(name: "Mcdonalds", amount: 6, user_id: 1, category_id: 1)
+entity2 = Entity.create(name: "Uber", amount: 13, user_id: 1, category_id: 2)
+entity3 = Entity.create(name: "Didi", amount: 12, user_id: 1, category_id: 2)
+entity4 = Entity.create(name: "Netflix", amount: 10, user_id: 1, category_id: 3)
+entity5 = Entity.create(name: "Spotify", amount: 10, user_id: 1, category_id: 3)
+entity6 = Entity.create(name: "Starbucks", amount: 5, user_id: 1, category_id: 1)
 
--- spec/views/categories/index.html.erb_spec.rb
+++ spec/views/categories/index.html.erb_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe 'categories/index', type: :view do
+RSpec.describe 'categories/index', type: :feature do
   before(:each) do
-    assign(:categories, [
-             Category.create!,
-             Category.create!
-           ])
+    visit new_user_session_path
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
   end
 
   it 'renders a list of categories' do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    expect(page).to have_selector('div.categories__list')
+  end
+
+  it 'renders the navbar' do
+    expect(page).to have_selector('nav.navbar')
+  end
+
+  it 'new category button redirect to new view' do
+    click_button 'NEW CATEGORY'
+    expect(current_path).to eq('/categories/new')
   end
 end
