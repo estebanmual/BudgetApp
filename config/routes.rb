a79fb95316bafe4227dc5ed8e301f3168c03f4a4
Estebanmual <esteban.munozal@gmail.com>
2022-07-01 09:28:47 -0500

    keep useful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'categories#splash'
   devise_for :users
-  resources :categories
+  resources :categories, only: %i[index show new create]
   resources :entities, only: %i[new create]
 end
