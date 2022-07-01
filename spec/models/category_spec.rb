37faa9105184e7ddbde0706fd72189975a3af20d
Estebanmual <esteban.munozal@gmail.com>
2022-06-30 22:08:01 -0500

    Add test category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid' do
+    category = Category.new(name: "Test", icon: "https://source.unsplash.com/random/?city,night", user_id: 1)
+    expect(category).to be_valid
+  end
+
+  it 'validates presence of name' do
+    category = Category.new(icon: "https://source.unsplash.com/random/?city,night", user_id: 1)
+    expect(category).to_not be_valid
+  end
+
+  it 'validates presence of icon' do
+    category = Category.new(name: "Test", icon:"", user_id: 1)
+    expect(category).to_not be_valid
+  end
 end
