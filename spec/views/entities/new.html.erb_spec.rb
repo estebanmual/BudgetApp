98f3949d9850907644b40008efd523468832444f
Estebanmual <esteban.munozal@gmail.com>
2022-07-01 11:14:13 -0500

    Add entities new view test

--- spec/views/entities/new.html.erb_spec.rb
+++ spec/views/entities/new.html.erb_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'entities/new', type: :view do
+RSpec.describe 'entities/new', type: :feature do
   before(:each) do
-    assign(:entity, Entity.new)
+    visit new_user_session_path
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    click_on 'NEW ENTITY'
   end
 
   it 'renders new entity form' do
-    render
+    expect(page).to have_selector('form.newEntity__form')
+  end
+  
+  it "renders the navbar" do
+    expect(page).to have_selector('nav.navbar')
+  end
 
-    assert_select 'form[action=?][method=?]', entities_path, 'post' do
-    end
+  it 'takes me back to categories index page' do
+    click_on '<'
+    expect(current_path).to eq('/categories')
   end
 end
