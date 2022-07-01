b2cc6d1c36684e2cd83b4c314c881b99805a0361
Estebanmual <esteban.munozal@gmail.com>
2022-07-01 09:27:39 -0500

    Keep actions we are using

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,25 +1,20 @@
 class CategoriesController < ApplicationController
   before_action :redirect
   skip_before_action :redirect, only: [:splash]
-  before_action :set_category, only: %i[show edit update destroy]
+  before_action :set_category, only: %i[show]
 
-  # GET /categories or /categories.json
   def index
     @categories = Category.includes(:entities).all
   end
 
-  # GET /categories/1 or /categories/1.json
+
   def show
     @entities = @category.entities.order(created_at: :desc)
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
-
-  # GET /categories/1/edit
-  def edit; end
 
   def create
     @category = current_user.categories.new(category_params)
@@ -28,29 +23,6 @@ class CategoriesController < ApplicationController
       redirect_to categories_path, notice: 'Category was successfully created.'
     else
       render :new, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
-  def destroy
-    @category.destroy
-
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -63,13 +35,11 @@ class CategoriesController < ApplicationController
   def redirect
     redirect_to root_path unless user_signed_in?
   end
-
-  # Use callbacks to share common setup or constraints between actions.
+  
   def set_category
     @category = Category.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def category_params
     params.require(:category).permit(:name, :icon)
   end
