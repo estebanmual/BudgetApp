da8b65d5435f901c8f4a5e59d17bc84706c14f0b
Estebanmual <esteban.munozal@gmail.com>
2022-07-01 11:10:06 -0500

    Add categories show view tests

--- spec/views/categories/show.html.erb_spec.rb
+++ spec/views/categories/show.html.erb_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'categories/show', type: :view do
+RSpec.describe 'categories/show', type: :feature do
   before(:each) do
-    @category = assign(:category, Category.create!)
+    visit new_user_session_path
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    click_on 'Food'
   end
 
-  it 'renders attributes in <p>' do
-    render
+  it 'renders list of entities' do
+    expect(page).to have_selector('div.entityList')
   end
+
+  it 'renders the navbar' do
+    expect(page).to have_selector('nav.navbar')
+  end
+
+  it 'new transaction redirect to new view' do
+    click_button 'Add a new transaction'
+    expect(current_path).to eq('/entities/new')
+  end
+
+  it 'takes me back to the categories index page' do
+    click_on '<'
+    expect(current_path).to eq('/categories')
+  end
+
 end
